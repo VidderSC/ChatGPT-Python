18dfb571524ba087e074fb7c27dfeb816966b826
David Som <103112459+VidderSC@users.noreply.github.com>
2023-09-30 23:45:56 +0000

    created file for lesson 4

---
+++ lesson4.py
@@ -0,0 +1,17 @@
+# Lesson 4: Functions and Modules
+
+# 4.1 Functions with Parameters
+
+# Create a function that calculates and returns the area of a rectangle.
+# The function should take two parameters, length and width, and return the area as a float.
+
+# 4.2 Modules
+
+# Create a separate Python file/module named geometry.py.
+# Move the rectangle area calculation function from the previous exercise into this module.
+# Import the geometry module into your current code and use the function to calculate the area of a rectangle.
+
+#4.3 Exercise
+
+# Create a function that checks if a given string is a palindrome (reads the same forwards and backward).
+# The function should take a string as input and return a boolean value (True if it’s a palindrome, False otherwise).
