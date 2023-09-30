af45eb39f0e9ec3d9f10d68bf4a4b7e2459227e5
David Som <103112459+VidderSC@users.noreply.github.com>
2023-09-30 22:59:07 +0000

    lesson3

---
+++ lesson3.py
@@ -0,0 +1,20 @@
+# Lesson 3: Control Flow and Loops
+
+# 3.1 If Statements
+
+# 	•	Write an if statement to check if a variable x is greater than 10.
+# If it is, print “x is greater than 10.”
+
+# 3.2 For Loops
+
+#	•	Create a list of numbers from 1 to 5.
+#	•	Use a for loop to print each number in the list.
+
+# 3.3 While Loops
+
+#	•	Write a while loop that counts from 1 to 3 and prints each number.
+
+# 3.4 Exercise
+
+#	•	Create a function that takes an integer n as input and prints all even numbers from 1 to n.
+# Use typed variables and return types.
