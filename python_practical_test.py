38275d8e5b32816e94947c4f3ca280fdf747f5a7
David Som <13154667-VidderSC@users.noreply.gitlab.com>
2023-10-01 20:52:38 +0200

    Python Practical Test

---
+++ python_practical_test.py
@@ -0,0 +1,58 @@
+# Python Practical Test
+
+# Instructions:
+# 1. Read each question carefully.
+# 2. Write your Python code to solve each problem.
+# 3. Feel free to use external resources (documentation, Google) as you normally would when programming.
+# 4. For each question, provide a comment with your explanation or thought process.
+# 5. Remember to use meaningful variable names and follow clean code principles.
+# 6. If you encounter any issues or uncertainties, you can indicate that in a comment.
+
+
+# Question 1:
+# Write a Python function that takes a list of numbers as input and returns the sum of all the even numbers in the list.
+
+
+
+# Question 2:
+# Create a class called "Person" with attributes for name, age, and city. Implement a method within the class that returns a string representation of the person's information.
+
+
+
+# Question 3:
+# Given a string, write a Python function that checks if it's a palindrome (reads the same forwards and backward). The function should return True if it's a palindrome, False otherwise.
+
+
+
+# Question 4:
+# Write a Python program that reads a CSV file named "data.csv" (you can create a sample CSV file) and prints the contents to the console.
+
+
+
+# Question 5:
+# Implement a Python function that takes a list of words as input and returns a dictionary where the keys are the words, and the values are the lengths of the words.
+
+
+
+# Question 6:
+# Create a Python class called "Rectangle" with attributes for length and width. Implement methods within the class to calculate the area and perimeter of the rectangle.
+
+
+
+# Question 7:
+# Write a Python program that uses a while loop to calculate the factorial of a given positive integer. The user should input the integer.
+
+
+
+# Question 8:
+# Given a list of numbers, write a Python function that returns a new list containing only the numbers that are divisible by 5.
+
+
+
+# Question 9:
+# Write a Python function that reads a text file named "sample.txt" (you can create a sample text file) and counts the number of words in the file.
+
+
+
+# Question 10:
+# Create a Python function that generates the Fibonacci sequence up to a specified number of terms. The user should input the number of terms.
