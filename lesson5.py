66d64cdd69e1a80ea9c872223f04be0a70bfd9f4
David Som <103112459+VidderSC@users.noreply.github.com>
2023-10-01 00:56:42 +0000

    created lesson 5 file

---
+++ lesson5.py
@@ -0,0 +1,15 @@
+# Lesson 5: Lists and List Comprehensions
+
+# 5.1 Lists
+# - Create a list of your favorite fruits.
+# - Print out the first and last items in the list.
+
+# 5.2 List Comprehensions
+# - Create a list of numbers from 1 to 10 using a list comprehension.
+# - Create a new list that contains only the even numbers from the
+# previous list, also using a list comprehension.
+
+# 5.3 Exercise
+# - Write a function that takes a list of numbers and returns a new
+# list containing only the numbers that are divisible by 3.
+# Use typed variables and return types.
