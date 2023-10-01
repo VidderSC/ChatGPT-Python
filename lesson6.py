24138fa737e1ec6c79cd21e0312b84fd00ec1243
David Som <13154667-VidderSC@users.noreply.gitlab.com>
2023-10-01 19:01:59 +0200

    finished lesson 6 exercises

---
+++ lesson6.py
@@ -0,0 +1,41 @@
+# Lesson 6: Strings and String Manipulation
+
+# 6.1 String Basics
+# - Create a string variable containing your full name.
+
+full_name = "Pepito Perez"
+
+# - Print out the length of your name (number of characters).
+
+print(len(full_name))
+
+# - Print out your name in uppercase.
+print(full_name.upper())
+
+# 6.2 String Slicing
+# - Extract your first name from your full name using string slicing.
+
+print(full_name[:6])
+
+# - Print out your last name using string slicing.
+print(full_name[7:])
+
+# - Print out your name in reverse.
+print(full_name[::-1])
+print()
+
+# 6.3 Exercise
+# - Write a function that takes a string as input and returns the same 
+# string with all spaces removed.
+# Use typed variables and return types.
+
+def remove_spaces(string: str) -> str:
+    no_spaces: str = ""
+    for letter in string:
+        if letter != " ":
+            no_spaces += letter
+    return no_spaces
+
+# Example usage:
+result = remove_spaces("Hello World")
+print(result)
